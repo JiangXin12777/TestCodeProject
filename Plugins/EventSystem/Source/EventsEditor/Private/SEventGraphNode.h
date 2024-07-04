89e15a0dff23c567ca2be9e1098e07a138fdfb0b
jiangxin <2649340476@qq.com>
2024-07-04 17:15:39 +0800

    ci: 上传EventSystem插件

---
+++ Plugins/EventSystem/Source/EventsEditor/Private/SEventGraphNode.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "KismetNodes/SGraphNodeK2Base.h"
+
+/**
+ * 
+ */
+class EVENTSEDITOR_API SEventGraphNode : public SGraphNodeK2Base
+{
+public:
+	SLATE_BEGIN_ARGS(SEventGraphNode) {}
+	SLATE_END_ARGS()
+
+	void Construct(const FArguments& InArgs, UEventsK2Node_EventBase* InNode);
+
+
+public:
+	virtual void CreatePinWidgets() override;
+};
